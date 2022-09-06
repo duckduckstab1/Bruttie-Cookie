0ad0915cd2fdfb5368372946641a5d4f8dda8302
enderop44 <85124822+enderop44@users.noreply.github.com>
2022-09-06 21:48:45 +0200

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+var options = {
+    strings: ['the best bruteforce 4 roblox.', 'a certefied pro',  'the certefied best 4 taking over.', 'Ender'],
+    typeSpeed: 100,
+    backSpeed:67,
+    loop: true
+  };
+var typed = new Typed('.typing', options);
